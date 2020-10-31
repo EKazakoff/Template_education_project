0b7163300f6b66bb2e6b3d7fc97956518d2fd670
Eugene Kazakov <ekazakov@gmail.com>
2020-10-31 11:18:19 +0300

    final syntax and data fixed for test DB

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS User (
     middle_name      VARCHAR(50)            COMMENT 'Отчество',
     position         VARCHAR(50)  NOT NULL  COMMENT 'Должность',
     phone            VARCHAR(15)            COMMENT 'Номер телефона',
-    doc_id           INTEGER      NOT NULL  COMMENT 'Внешний ключ на документ, удостоверяющий личность',
+    user_doc_id      INTEGER      NOT NULL  COMMENT 'Внешний ключ на документ, удостоверяющий личность',
     citizenship_code INTEGER      NOT NULL  COMMENT 'Внешний ключ на документ, содержащий код страны',
     is_identified    BOOLEAN                COMMENT 'Определён/не определён'
 );
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS User_document (
     id              INTEGER                 COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
     doc_code        INTEGER       NOT NULL  COMMENT 'Ссылка на справочник',
     doc_number      INTEGER       NOT NULL  COMMENT 'Номер документа',
-    doc_data        TIMESTAMP     NOT NULL  COMMENT 'Дата документа',
+    doc_data        TIMESTAMP     NOT NULL  COMMENT 'Дата документа'
 );
 COMMENT ON TABLE User_document IS 'Документ пользователя';
 
@@ -23,11 +23,11 @@ CREATE TABLE IF NOT EXISTS Organization (
     id               INTEGER                COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
     name             VARCHAR(128) NOT NULL  COMMENT 'Название организации',
     full_org_name    VARCHAR(128) NOT NULL  COMMENT 'Полное название организации',
-    inn              INTEGER      NOT NULL  COMMENT 'ИНН организации',
+    inn              BIGINT       NOT NULL  COMMENT 'ИНН организации',
     kpp              INTEGER      NOT NULL  COMMENT 'КПП организации',
     address          VARCHAR(128) NOT NULL  COMMENT 'Адрес организации',
     phone            VARCHAR(15)            COMMENT 'Номер телефона ',
-    is_active        BOOLEAN                COMMENT 'Действующий/не действующий',
+    is_active        BOOLEAN                COMMENT 'Действующий/не действующий'
 );
 COMMENT ON TABLE Organization IS 'Организация';
 
@@ -35,8 +35,8 @@ CREATE TABLE IF NOT EXISTS Office (
     id               INTEGER               COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
     name             VARCHAR(128) NOT NULL COMMENT 'Название офиса',
     address          VARCHAR(128) NOT NULL COMMENT 'Адрес офиса',
-    phone            INTEGER               COMMENT 'Номер телефона',
-    is_active        BOOLEAN               COMMENT 'Действующий/не действующий',
+    phone            VARCHAR(15)               COMMENT 'Номер телефона',
+    is_active        BOOLEAN               COMMENT 'Действующий/не действующий'
 );
 COMMENT ON TABLE Office IS 'Офис';
 
@@ -57,6 +57,6 @@ CREATE INDEX IX_User_document_Id ON User_document (id);
 CREATE INDEX IX_Organization_Id ON Organization (id);
 CREATE INDEX IX_Office_Id ON Office (id);
 
-ALTER TABLE User ADD FOREIGN KEY (doc_id) REFERENCES User_document(id);
+ALTER TABLE User ADD FOREIGN KEY (user_doc_id) REFERENCES User_document(id);
 ALTER TABLE User ADD FOREIGN KEY (citizenship_code) REFERENCES Country(code);
 ALTER TABLE User_document ADD FOREIGN KEY (doc_code) REFERENCES Document(code);
