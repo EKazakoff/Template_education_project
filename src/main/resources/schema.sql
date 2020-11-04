ae05f4295fda775ad464b567c783b7fc88d3d7c8
Eugene Kazakov <ekazakov@gmail.com>
2020-11-04 21:00:58 +0300

    .gitignore was modified

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,14 +20,14 @@ CREATE TABLE IF NOT EXISTS User_document (
 COMMENT ON TABLE User_document IS 'Документ пользователя';
 
 CREATE TABLE IF NOT EXISTS Organization (
-    id               INTEGER                COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-    name             VARCHAR(128) NOT NULL  COMMENT 'Название организации',
-    full_org_name    VARCHAR(128) NOT NULL  COMMENT 'Полное название организации',
-    inn              BIGINT       NOT NULL  COMMENT 'ИНН организации',
-    kpp              INTEGER      NOT NULL  COMMENT 'КПП организации',
-    address          VARCHAR(128) NOT NULL  COMMENT 'Адрес организации',
-    phone            VARCHAR(15)            COMMENT 'Номер телефона ',
-    is_active        BOOLEAN                COMMENT 'Действующий/не действующий'
+    id              INTEGER                COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(128) NOT NULL  COMMENT 'Название организации',
+    full_org_name   VARCHAR(128) NOT NULL  COMMENT 'Полное название организации',
+    inn             VARCHAR(12)  NOT NULL  COMMENT 'ИНН организации',
+    kpp             VARCHAR(12)  NOT NULL  COMMENT 'КПП организации',
+    address         VARCHAR(128) NOT NULL  COMMENT 'Адрес организации',
+    phone           VARCHAR(15)            COMMENT 'Номер телефона ',
+    is_active       BOOLEAN                COMMENT 'Действующий/не действующий'
 );
 COMMENT ON TABLE Organization IS 'Организация';
 
@@ -35,20 +35,22 @@ CREATE TABLE IF NOT EXISTS Office (
     id               INTEGER               COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
     name             VARCHAR(128) NOT NULL COMMENT 'Название офиса',
     address          VARCHAR(128) NOT NULL COMMENT 'Адрес офиса',
-    phone            VARCHAR(15)               COMMENT 'Номер телефона',
+    phone            VARCHAR(15)           COMMENT 'Номер телефона',
     is_active        BOOLEAN               COMMENT 'Действующий/не действующий'
 );
 COMMENT ON TABLE Office IS 'Офис';
 
 CREATE TABLE IF NOT EXISTS Document (
-    name            VARCHAR(256)          COMMENT 'Тип документа',
-    code            INTEGER               COMMENT 'Код документа'
+    id              INTEGER                COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(256)           COMMENT 'Тип документа',
+    code            INTEGER                COMMENT 'Код документа'
 );
 COMMENT ON TABLE Document IS 'Документ';
 
 CREATE TABLE IF NOT EXISTS Country (
-    name           VARCHAR(256)           COMMENT 'Страна, выдавшая документ',
-    code           INTEGER                COMMENT 'Код страны, выдавшей документ'
+    id              INTEGER                COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(256)           COMMENT 'Страна, выдавшая документ',
+    code            INTEGER                COMMENT 'Код страны, выдавшей документ'
 );
 COMMENT ON TABLE Country IS 'Страна';
 
@@ -58,5 +60,5 @@ CREATE INDEX IX_Organization_Id ON Organization (id);
 CREATE INDEX IX_Office_Id ON Office (id);
 
 ALTER TABLE User ADD FOREIGN KEY (user_doc_id) REFERENCES User_document(id);
-ALTER TABLE User ADD FOREIGN KEY (citizenship_code) REFERENCES Country(code);
-ALTER TABLE User_document ADD FOREIGN KEY (doc_code) REFERENCES Document(code);
+ALTER TABLE User ADD FOREIGN KEY (citizenship_code) REFERENCES Country(id);
+ALTER TABLE User_document ADD FOREIGN KEY (doc_code) REFERENCES Document(id);
