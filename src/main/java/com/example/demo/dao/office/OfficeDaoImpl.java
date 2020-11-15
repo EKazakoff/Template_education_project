8d69effc43e69de9ff32740c97e09718ddd822e8
Eugene Kazakov <ekazakov@gmail.com>
2020-11-15 14:46:04 +0300

    refactoring bug was fixed and office controller implementation was made

--- src/main/java/com/example/demo/dao/office/OfficeDaoImpl.java
+++ src/main/java/com/example/demo/dao/office/OfficeDaoImpl.java
@@ -1,12 +1,22 @@
 package com.example.demo.dao.office;
 
 import com.example.demo.model.Office;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+
 @Repository
-public class OfficeDaoImpl implements OfficeDao{
+public class OfficeDaoImpl implements OfficeDao {
+    private final EntityManager em;
+
+    @Autowired
+    public OfficeDaoImpl(EntityManager em) {
+        this.em = em;
+    }
+
     @Override
     public Office loadById(Long id) {
-        return null;
+        return em.find(Office.class, id);
     }
 }
--- src/main/java/com/example/demo/model/Office.java
+++ src/main/java/com/example/demo/model/Office.java
@@ -1,4 +1,63 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "Office")
 public class Office {
+    @Id
+    private Long id;
+    private String name;
+    private String orgId;
+    private String address;
+    private String phone;
+    private Boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 }
